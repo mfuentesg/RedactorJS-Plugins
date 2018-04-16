12769991fb83f0bcaafaf37439b4cb2b3c2f7891
Marcelo Fuentes <mfuentesg@users.noreply.github.com>
2018-04-16 11:38:22 -0300

    Update redactor.fontsize.js

--- redactor.fontsize.js
+++ redactor.fontsize.js
@@ -3,11 +3,9 @@
 if (!RedactorPlugins) var RedactorPlugins = {};
 
 RedactorPlugins.fontsize = {
-  init: () => {
-    const _this = this;
-    
-    _this.buttonAdd('fontSize-up', 'Increase font-size', _this.increaseFontSize);
-    _this.buttonAdd('fontSize-down', 'Decrease font-size', _this.decreaseFontSize);
+  init: () => {   
+    this.buttonAdd('fontSize-up', 'Increase font-size', _this.increaseFontSize);
+    this.buttonAdd('fontSize-down', 'Decrease font-size', _this.decreaseFontSize);
   },
   increaseFontSize: () => {
     this.applyFontSize(2);
@@ -19,13 +17,12 @@ RedactorPlugins.fontsize = {
     return parseInt($(this.getElement()).css('fontSize'), 10);
   },
   applyFontSize: (value) => {
-    const _this = this;
-    const currentSize = _this.getCurrentSize();
+    const currentSize = this.getCurrentSize();
 
     value = value ? (currentSize + value) : currentSize;
 
     if (value > 10 && value <= 48) {
-      _this.inlineSetStyle('font-size', value + 'px');
+      this.inlineSetStyle('font-size', value + 'px');
     }
   }
 };
